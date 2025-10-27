cfea061c1d6b621220875c8638151cb1e78ab845
anaclaireellen <anaclaireellen.naranjo@cit.edu>
2025-10-28 01:18:20 +0800

    trial and error

---
+++ backend/src/main/java/com/example/syncup/controller/RoleController.java
@@ -0,0 +1,21 @@
+package com.example.syncup.controller;
+
+import com.example.syncup.entity.Role;
+import com.example.syncup.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping
+    public Role createRole(@RequestBody Role role) {
+        return roleService.saveRole(role);
+    }
+    
+    //for trial and error
+}
