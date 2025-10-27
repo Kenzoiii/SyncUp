3a324d175a3baae0f7a5d7507c4dc9ee52f7bef2
anaclaireellen <anaclaireellen.naranjo@cit.edu>
2025-10-27 23:33:23 +0800

    added role and team files on other folders

---
+++ backend/src/main/java/com/example/syncup/controller/TeamController.java
@@ -0,0 +1,21 @@
+package com.example.syncup.controller;
+
+import com.example.syncup.entity.Team;
+import com.example.syncup.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/teams")
+public class TeamController {
+
+    @Autowired
+    private TeamService teamService;
+
+    @PostMapping
+    public Team createTeam(@RequestBody Team team) {
+        return teamService.saveTeam(team);
+    }
+
+    // Example: GET method for listing all teams would go here
+}
